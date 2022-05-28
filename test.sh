03e65bfe151ff9f3b77785e01f2397a10a2aa4ab
Caruychen <caruychen@gmail.com>
2022-05-28 16:25:49 +0300

    modified binary

---
+++ test.sh
@@ -0,0 +1,12 @@
+cargo build --release
+cp ./target/release/minisynth ./
+#./target/release/minisynth
+#./target/release/minisynth file1 file2
+#./target/release/minisynth audio_samples/Simple_Tune.synth
+#./target/release/minisynth audio_samples/Prelude.synth
+#./target/release/minisynth audio_samples/Imperial_March.synth
+#./target/release/minisynth audio_samples/Super_Mario.synth
+#./target/release/minisynth audio_samples/Cantina_Band.synth
+#./target/release/minisynth audio_samples/Toccata.synth
+#./target/release/minisynth audio_samples/Nightcall.synth
+./target/release/minisynth audio_samples/NeverGonnaGiveYouUp.synth
